23336b507af02e83a246006a2804bcee8ae84c9f
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-03-17 03:34:52 +0000

    Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
+int main(void)
+{
+	int c
+	c = write(1,"and that piece of art is useful" - Dora Korpar, 2015-10-19"\n");
+	return (1);
+}
