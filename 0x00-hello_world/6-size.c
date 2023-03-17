3c0faa0e300b2bc0c2dd101eeae675b2b29def8b
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-03-17 03:37:06 +0000

    Corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int integerType;
-    float floatType;
-    long longType;
-    long long longerType;
-    char charType;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-    // Sizeof operator is used to evaluate the size of a variable
-    printf ("Size of char: %i byte(s)\n", sizeof(char));
-    printf("Size of int: %i byte(s)\n",sizeof(int));
-    printf("Size of long int: %i byte(s)\n", sizeof(long));
-    printf("Size of long long int: %i byte(s)\n", sizeof(double));
-    printf("Size of float: %i bytes\n",sizeof(float));
-
-    return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
