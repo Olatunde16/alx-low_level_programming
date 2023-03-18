a6bc7f39afa27e3149f72ccddcfbbe03208f8e44
Olutunmibi Iseoluwa Olatunde <olatundeolutunmibi@gmail.com>
2023-03-17 19:48:12 -0700

    Corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int c
-	c = write(1,"and that piece of art is useful" - Dora Korpar, 2015-10-19"\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
