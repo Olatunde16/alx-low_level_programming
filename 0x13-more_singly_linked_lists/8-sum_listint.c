e8ddc27f7a42a894dd2c676b05e32316806629ab
Ahmed Ashrys <olatundeolutunmibi@gmail.com>
2023-05-01 17:56:23 -0700

    Task 8 Completed

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,16 +1,15 @@
 #include "lists.h"
 
 /**
- * sum_listint - calculates the sum of all the data in a listint_t lis
-t
+ * sum_listint - calculates the sum of all the data in a listint_t list
  * @head: first node in the linked list
  *
- * Return: resulting sum, or 0 if list is empty
+ * Return: resulting sum, or 0 if list is not empty
  */
 
 int sum_listint(listint_t *head)
-{       
-        int sum = 0;
+{
+	int sum = 0;
 	listint_t *temp = head;
 
 	while (temp)
