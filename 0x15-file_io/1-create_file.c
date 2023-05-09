0d5ab713d171030a6e77fae4ad0e63c1ebd785eb
Ahmed Ashrys <olatundeolutunmibi@gmail.com>
2023-05-08 17:53:44 -0700

    Updated Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	O = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	O = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	W = write(O, text_content, len);
 
 	if (O == -1 || W == -1)
